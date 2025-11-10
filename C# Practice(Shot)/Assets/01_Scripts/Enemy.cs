7b120251a1be320d0994aed69639d85d3b95e8cd
asonost97-oss <asonost97@gmail.com>
2025-11-10 21:51:00 +0900

    Day 21 Shooting Game
    Day 21 Shooting Game

---
+++ C# Practice(Shot)/Assets/01_Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    string enemy;
+
+    int speed = 1;
+
+    int power = 10;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
