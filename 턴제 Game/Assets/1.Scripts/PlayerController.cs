2efc7094561288453b510490ca18a7bd4a1ea7b3
asonost97-oss <asonost97@gmail.com>
2025-11-19 21:53:32 +0900

    Update PlayerController.cs

--- 턴제 Game/Assets/1.Scripts/PlayerController.cs
+++ 턴제 Game/Assets/1.Scripts/PlayerController.cs
@@ -3,17 +3,4 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public PlayerData PlayerData;
-
-    
-    
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
